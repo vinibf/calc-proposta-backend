1412b8adc631a6734ce4a62dc3bf70fe31ec4471
Vinicius BF <vini91bf@gmail.com>
2021-08-11 19:06:04 -0300

    Create modules user and proposal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ProposalModule } from './proposal/proposal.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ProposalModule],
   controllers: [AppController],
   providers: [AppService],
 })
