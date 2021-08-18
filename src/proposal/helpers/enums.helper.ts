76645cc7b04f3ccd8b881e69fd56346859887b35
Vinicius BF <vini91bf@gmail.com>
2021-08-18 16:49:20 -0300

    Created enum helper to use inside proposal entity

---
+++ src/proposal/helpers/enums.helper.ts
@@ -0,0 +1,13 @@
+enum PowerSupplyEnum {
+  CONVENCIONAL = 5,
+  RENOVAVEL = -2,
+}
+
+enum SubmarketEnum {
+  NORTE = 2,
+  NORDESTE = -1,
+  SUL = 3.5,
+  SUDESTE = 1.5,
+}
+
+export { PowerSupplyEnum, SubmarketEnum };
