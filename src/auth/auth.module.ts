08a1cab7f197519dba98cafc19e328d7c2f672a2
Helama-Silva <helama.a.silva@gmail.com>
2021-08-18 19:32:15 -0300

    Guard Service JWT

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from '../user/user.module';
+import { AuthService } from './shared/auth.service';
+import { jwtConstants } from './constants/constants';
+import { JwtStrategy } from './shared/jwt.strategy';
+
+@Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '6000s' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
---
+++ src/auth/constants/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: '0m3g4-pr0j3ct',
+};
