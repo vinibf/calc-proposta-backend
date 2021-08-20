0e7bbb1c5c6e17f1f885a26de8cefb00916226fa
Vinicius BF <vini91bf@gmail.com>
2021-08-20 05:26:56 -0300

    Added TypeOrm config, proposal provider and proposal controller

--- src/proposal/proposal.module.ts
+++ src/proposal/proposal.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProposalController } from './controllers/proposal.controller';
+import { Proposal } from './entities/proposal.entity';
+import { ProposalService } from './services/proposal.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Proposal])],
+  exports: [TypeOrmModule],
+  providers: [ProposalService],
   controllers: [ProposalController],
 })
 export class ProposalModule {}
