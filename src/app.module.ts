58a5ee85667296e6f258cec1227bee87c09f893b
Vinicius BF <vini91bf@gmail.com>
2021-08-20 05:23:43 -0300

    Added TypeOrm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { ProposalModule } from './proposal/proposal.module';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [UserModule, ProposalModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, ProposalModule],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
