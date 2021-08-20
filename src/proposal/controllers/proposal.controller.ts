0b360e6efced0323c75346561f072dd1f7998315
Vinicius BF <vini91bf@gmail.com>
2021-08-20 05:31:22 -0300

    Adjusted the endpoints in proposal controller

--- src/proposal/controllers/proposal.controller.ts
+++ src/proposal/controllers/proposal.controller.ts
@@ -6,36 +6,45 @@ import {
   Param,
   Patch,
   Post,
-  Redirect
 } from '@nestjs/common';
+import { Guid } from 'guid-typescript';
 import { CreateProposalDto } from '../dtos/create-proposal.dto';
 import { UpdateProposalDto } from '../dtos/update-proposal.dto';
+import { Proposal } from '../entities/proposal.entity';
+import { ProposalService } from '../services/proposal.service';
 
 @Controller('proposals')
 export class ProposalController {
+  constructor(private service: ProposalService) {}
+
   @Post()
   //@Redirect('url', 200)
-  async create(@Body() createProposalDto: CreateProposalDto) {
-    return 'created';
+  async create(
+    @Body() createProposalDto: CreateProposalDto,
+  ): Promise<Proposal> {
+    return this.service.create(createProposalDto);
   }
 
   @Get()
   async findAll(): Promise<Proposal[]> {
-    return [];
+    return this.service.findAll();
+  }
+
+  @Get(':public_id')
+  async findOne(@Param('public_id') id: Guid): Promise<Proposal> {
+    return this.service.findOne(id);
   }
 
   @Patch(':public_id')
-  update(
-    @Param('public_id') id: string,
-    @Body() updateProposalDto: UpdateProposalDto
-  ) {
-    console.log(id);
-    return 'updated';
+  async certificate(
+    @Param('public_id') id: Guid,
+    @Body() updateProposalDto: UpdateProposalDto,
+  ): Promise<Proposal> {
+    return this.service.certificate(id, updateProposalDto);
   }
 
   @Delete(':public_id')
-  delete(@Param('public_id') id: string) {
-    console.log(id);
-    return 'deleted';
+  async delete(@Param('public_id') id: Guid): Promise<Proposal> {
+    return this.service.delete(id);
   }
 }
